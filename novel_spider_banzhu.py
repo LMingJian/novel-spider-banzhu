5eabc2ffae8f9b6cd6bcfb75fc8e5ad96fc1f66f
LMingJian <Liang1312635752@outlook.com>
2025-12-26 15:24:58 +0800

    2025-12-26

--- novel_spider_banzhu.py
+++ novel_spider_banzhu.py
@@ -1,4 +1,5 @@
 import sys
+from urllib.parse import urljoin
 
 import bs4
 from bs4 import BeautifulSoup
@@ -13,86 +14,41 @@ from tools import WebDriver, Replace
 
 class SpiderBanZhu:
 
-    def __init__(self, driver_path: str, driver_option: list):
-        webdriver = WebDriver(driver_path, driver_option)
-        self._browser = webdriver.start_browser()
-        self._replace = Replace(r'./replace/replacePictureFont.json',
-                                r'./replace/replaceIconFont.json')
-        self.cf_clearance = ''
-        self.cf_referer = ''
-        self.baseurl = ''
-        self.default = 6
-        self._menu()
+    def __init__(self, driver_path: str, driver_option: list=None):
+        self.webdriver = WebDriver(driver_path, driver_option)
+        self.browser = None
+        self.cf_clearance = None
+        self.cf_referer = None
+        self.baseurl = None
+        self.default = 3
+        self.replace = Replace(r'./replace/replacePictureFont.json', r'./replace/replaceIconFont.json')
+        # 启动菜单
+        self.menu()
 
-    def _find_element(self, css):
-        try:
-            element = self._browser.find_element(By.CSS_SELECTOR, css)
-            return element
-        except BaseException as e:
-            print(e)
-            sys.exit('_find_element 元素定位异常，请检查，程序退出！')
-
-    def _find_elements(self, css):
-        try:
-            element = self._browser.find_elements(By.CSS_SELECTOR, css)
-            return element
-        except BaseException as e:
-            print(e)
-            sys.exit('_find_elements 元素定位异常，请检查，程序退出！')
-
-    def _check(self):
-        """检查页面是否正常（尝试 5 次，10 s 每 2 s 一次）"""
-        print('检查页面正常...')
-        time.sleep(self.default)
-        try:
-            WebDriverWait(
-                self._browser, 10, 1
-            ).until(
-                ec.visibility_of_element_located((By.CSS_SELECTOR, '.logo img'))
-            )
-            WebDriverWait(
-                self._browser, 10, 1
-            ).until(
-                ec.visibility_of_element_located((By.CSS_SELECTOR, '.container'))
-            )
-        except BaseException as e:  # noqa
-            print(e)
-            sys.exit('_check 页面异常，请检查，程序退出！')
-
-    def _verify(self, url):
-        print("进行验证绕过！")
-        self._browser.get(url)
-        self._browser.execute_script('window.stop()')
-        self._browser.add_cookie({'name': 'cf_clearance', 'value': self.cf_clearance})
-        self._browser.get(url + '/?__cf_chl_tk=' + self.cf_referer)
-        try:
-            print('检查登录状态！')
-            WebDriverWait(
-                self._browser, self.default, 1
-            ).until(
-                ec.visibility_of_element_located((By.CSS_SELECTOR, '.logo img'))
-            )
-        except BaseException:  # noqa
-            print('未登录，执行登录操作！')
-            self._find_element('#password').send_keys('1234')
-            time.sleep(1)
-            self._find_element('.login').click()
-            time.sleep(3)
-        self._check()
-
-    def _menu(self):
+    def menu(self):
         print("====================")
         print("欢迎进入系统")
         print("====================")
         print(f'请指定网站地址 https://????? ')
         self.baseurl = input('url: ')
-        print(f'请手动获取 cf_clearance ')
-        self.cf_clearance = input('cf_clearance: ')
-        print(f'请手动获取 cf_referer ')
-        self.cf_referer = input('cf_referer: ')
+        print(f'是否具有 Cloudflare 盾？')
+        if input('Yes(y) or No(n,Enter): ') == 'y':
+            print(f'请手动获取 cf_clearance ')
+            self.cf_clearance = input('cf_clearance: ')
+            print(f'请手动获取 cf_referer ')
+            self.cf_referer = input('cf_referer: ')
+        else:
+            pass
         print("====================")
         print('系统初始化。。。')
-        self._verify(self.baseurl)
+        # 启动浏览器
+        self.browser = self.webdriver.start_browser()
+        if self.cf_clearance:
+            # 绕过验证
+            self.verify()
+        else:
+            self.browser.get(self.baseurl)
+            self.check()
         print('初始化成功')
         print("====================")
         print('1.搜索')
@@ -102,29 +58,91 @@ class SpiderBanZhu:
         while True:
             flag = input('请选择功能: ')
             if flag == '1':
-                self._search()
+                self.search()
             elif flag == '2':
-                self._download()
+                self.download()
             elif flag == '6':
-                self._browser.quit()
+                self.browser.quit()
                 break
             else:
                 print('无此功能')
                 continue
         print('退出系统')
 
-    def _search(self):
+    def verify(self):
+        print("进行验证绕过！")
+        self.browser.get(self.baseurl)
+        self.browser.execute_script('window.stop()')
+        self.browser.add_cookie({'name': 'cf_clearance', 'value': self.cf_clearance})
+        self.browser.get(urljoin(self.baseurl, f'/?__cf_chl_tk={self.cf_referer}'))
+        try:
+            print('检查登录状态！')
+            # 检查左上角是否有 LOGO
+            WebDriverWait(
+                self.browser, self.default, 1
+            ).until(
+                ec.visibility_of_element_located((By.CSS_SELECTOR, '.logo img'))
+            )
+        except BaseException:  # noqa
+            print('未登录，执行登录操作！')
+            self.find_element('#password').send_keys('1234')
+            time.sleep(1)
+            self.find_element('.login').click()
+            time.sleep(3)
+        # 检查网站是否正常
+        self.check()
+
+    def find_element(self, css, strong=True):
+        try:
+            element = self.browser.find_element(By.CSS_SELECTOR, css)
+            return element
+        except BaseException as e:
+            print(e)
+            if not strong:
+                return None
+            sys.exit('find_element 元素定位异常，请检查，程序退出！')
+
+    def find_elements(self, css, strong=True):
+        try:
+            element = self.browser.find_elements(By.CSS_SELECTOR, css)
+            return element
+        except BaseException as e:
+            print(e)
+            if not strong:
+                return None
+            sys.exit('find_elements 元素定位异常，请检查，程序退出！')
+
+    def check(self):
+        """检查页面是否正常（尝试 5 次，10 s 每 2 s 一次）"""
+        print('检查页面正常...')
+        time.sleep(self.default)
+        try:
+            WebDriverWait(
+                self.browser, 10, 1
+            ).until(
+                ec.visibility_of_element_located((By.CSS_SELECTOR, '.logo img'))
+            )
+            WebDriverWait(
+                self.browser, 10, 1
+            ).until(
+                ec.visibility_of_element_located((By.CSS_SELECTOR, '.container'))
+            )
+        except BaseException as e:  # noqa
+            print(e)
+            sys.exit('check 页面异常，请检查，程序退出！')
+
+    def search(self):
         key = input('请输入关键字(exit 退出): ')
         if key == 'exit':
             return 0
-        self._find_element('.text-border').send_keys(key)
+        self.find_element('.text-border').send_keys(key)
         time.sleep(2)
-        self._find_element('.btn').click()
+        self.find_element('.btn').click()
         while True:
-            self._check()
+            self.check()
             chap_text = []
             chap_link = []
-            link = self._find_elements('li.column-2 > div > a:nth-child(1)')
+            link = self.find_elements('li.column-2 > div > a:nth-child(1)')
             for each in link:
                 chap_text.append(each.text)
                 chap_link.append(each.get_attribute('href'))
@@ -133,23 +151,29 @@ class SpiderBanZhu:
             for each in range(len(chap_text)):
                 print(f'{chap_text[each]} | {chap_link[each]}')
             print('====================')
-            next_page = input('前往下一页(q 退出): ')
+            next_page = input('q 退出: ')
             if next_page == 'q':
                 print('结束搜索')
                 print('====================')
                 break
             else:
-                self._find_element('.nextPage').click()
-                continue
+                ele = self.find_element('.nextPage', strong=False)
+                if ele:
+                    ele.click()
+                    continue
+                break
 
-    def _download(self):
+    def download(self):
         url = input('请输入链接 /?????/ (exit 退出): ')
         if url == 'exit':
             return 0
-        self._browser.get(self.baseurl + url)
-        self._check()
-        novel_name = self._find_element('.right > h1:nth-child(1)').text
-        novel_author = self._find_element('p.info').text.split('\n')[0].replace('作者：', '')
+        if self.baseurl in url:
+            self.browser.get(url)
+        else:
+            self.browser.get(urljoin(self.baseurl, url))
+        self.check()
+        novel_name = self.find_element('.right > h1:nth-child(1)').text
+        novel_author = self.find_element('p.info').text.split('\n')[0].replace('作者：', '')
         print("====================")
         print(novel_name)
         print(novel_author)
@@ -161,18 +185,18 @@ class SpiderBanZhu:
         catalog = self.get_catalog()
         print("====================")
         print(f'总共 {len(catalog)} 章')
-        target = input('请输入开始章节，从 0 开始：')
+        target = input('请输入开始章节，从 1 开始：')
         try:
-            target = int(target)
+            target = int(target) - 1
         except BaseException:  # noqa
             target = 0
         print("====================")
         print("下载开始")
         for value in catalog[target].values():
             url = value
-        self._browser.get(url)
+        self.browser.get(url)
         while True:
-            self._check()
+            self.check()
             content = self.get_content()
             with open(f'./result/[{novel_author}] {novel_name}{target}.txt', 'wb') as f:
                 for _ in content:
@@ -182,7 +206,7 @@ class SpiderBanZhu:
             if target < len(catalog):
                 for value in catalog[target].values():
                     url = value
-                self._browser.get(url)
+                self.browser.get(url)
             else:
                 print('下载结束')
                 print("====================")
@@ -191,86 +215,47 @@ class SpiderBanZhu:
     def get_catalog(self):
         catalog = []
         flag = 0
-        page = -1
         while True:
-            node = self._find_elements('div.mod:nth-child(7) a')
+            node = self.find_elements('div.mod:nth-child(7) a')
             for each in node:
                 catalog_name = f'【{flag}】{each.text}'
                 catalog_link = each.get_attribute('href')
+                if self.baseurl not in catalog_link:
+                    catalog_link = urljoin(self.baseurl, catalog_link)
                 catalog.append({catalog_name: catalog_link})
                 flag += 1
-            _pre = self._find_element('.prePage')
-            _next = self._find_element('.nextPage')
-            _end = self._find_element('.endPage')
-            if _next.get_attribute('href') == _end.get_attribute('href'):
-                if _end.get_attribute('href') != _pre.get_attribute('href') and page != 0:
-                    page += 1
-                else:
-                    break
-            _next.click()
-            print('获取章节中...')
-            self._check()
+            print('请确认是否存在分页，并跳转到下一页')
+            if input('Yes(y) or No(n,Enter): ') == 'y':
+                self.check()
+                print('获取章节中...')
+                continue
+            else:
+                break
         return catalog
 
-    def _check_append(self):
-        try:
-            self._browser.find_element(By.CSS_SELECTOR, 'div.neirong div + i')
-            return 1
-        except BaseException:  # noqa
-            self._browser.refresh()
-            time.sleep(self.default)
-        try:
-            self._browser.find_element(By.CSS_SELECTOR, 'div.neirong div + i')
-            return 1
-        except BaseException:  # noqa
-            return 0
-
-    def _check_pages(self, page_number):
-        for i in range(5):
-            page = self._browser.find_elements(By.CSS_SELECTOR, 'center.chapterPages a')
-            if page_number != len(page):
-                self._browser.refresh()
-                time.sleep(self.default)
-            else:
-                return 1
-        sys.exit('_check_pages 页面异常，请检查，程序退出！')
-
     def get_content(self):
-        page_number = len(self._find_elements('center.chapterPages a'))
-        print(self._browser.current_url)
+        page_number = len(self.find_elements('center.chapterPages a'))
+        print(self.browser.current_url)
         print(f'分页：{page_number}')
         if page_number == 0:
             content_page = range(1)
         else:
-            content_page = range(page_number)  # 分页
-        content = []
+            content_page = range(page_number+1)  # 分页
+        content = None
         for _ in content_page:
             print(f'当前：{_}')
-            if len(self._find_elements('#chapterinfo')) != 0:
-                content_html = self._find_element('#chapterinfo')
-                content = self.content_decode(content_html.get_attribute("outerHTML"), 'chapterinfo')
-            elif len(self._find_elements('#ad')) != 0:
-                content_html = self._find_element('#ad')
-                new_content = self.content_decode(content_html.get_attribute("outerHTML"), 'ad')
-                content = content + new_content
-            else:
-                flag = 1 if "$('.neirong').append(e);" in self._browser.page_source else 0
-                if flag:
-                    if_append = self._check_append()
-                    content_html = self._find_element('div.neirong')
-                    new_content = self.content_decode(content_html.get_attribute("outerHTML"), 'neirong', 'class')
-                    if if_append:
-                        print('警告：有缺漏！')
-                        new_content = new_content + [f'\n<!-- Append {self._browser.current_url} -->\n']
+            content_html = self.find_element('#nr1', strong=False)
+            if content_html:
+                if content is None:
+                    content = self.content_decode(content_html.get_attribute("outerHTML"), 'nr1')
                 else:
-                    content_html = self._find_element('div.neirong')
-                    new_content = self.content_decode(content_html.get_attribute("outerHTML"), 'neirong', 'class')
-                content = content + new_content
-            if _ + 1 < len(content_page):
-                self._check_pages(page_number)
-                next_page = self._find_elements('center.chapterPages a')[_ + 1]
-                next_page.click()
-                self._check()
+                    new_content = self.content_decode(content_html.get_attribute("outerHTML"), 'nr1')
+                    content += new_content
+            if _ == page_number:
+                break
+            next_page = self.find_elements('center.chapterPages a')[_]
+            next_page.click()
+            self.check()
         return content
 
     def content_decode(self, content_html, target, target_type='id'):
@@ -294,13 +279,17 @@ class SpiderBanZhu:
                     content_temp = ''
             elif _.name == 'img':
                 img_id = _.get('src').replace('/toimg/data/', '')
-                content_temp = content_temp + self._replace.picture_font_reverse(img_id)
+                content_temp = content_temp + self.replace.picture_font_reverse(img_id)
             elif _.name == 'i':
                 iconfont = _.contents[0]
                 icon_id = repr(iconfont).replace(r"\ue", "").replace('\'', '')
-                content_temp = content_temp + self._replace.icon_font_reverse(icon_id)
+                content_temp = content_temp + self.replace.icon_font_reverse(icon_id)
             elif _.name == 'div':
                 content = content + self.content_decode(_, '')
         if (content_temp not in content) and (content_temp != ''):
             content.append(content_temp)
         return content
+
+if __name__ == '__main__':
+    SpiderBanZhu(r'F:\Sdk\webdriver\geckodriver.exe')
+    # 测试地址: https://m.05banzhu.store/
--- tools/webdriver.py
+++ tools/webdriver.py
@@ -1,30 +1,40 @@
 from selenium.webdriver import Chrome, ChromeOptions
 from selenium.webdriver import Edge, EdgeOptions
-from selenium.webdriver import Firefox, FirefoxOptions
+from selenium.webdriver import Firefox, FirefoxOptions, FirefoxService
 from selenium.webdriver.chrome.service import Service
 
 
 class WebDriver:
 
-    def __init__(self, driver_path: str, driver_options: list):
+    def __init__(self, driver_path: str, driver_options: list=None):
+        if driver_options is None:
+            driver_options = []
         if 'chromedriver' in driver_path:
             self._options = ChromeOptions()
             self._webdriver = Chrome
+            self._service = Service(executable_path=driver_path)
+            print('Current Driver Chrome')
         elif 'msedgedriver' in driver_path:
             self._options = EdgeOptions()
             self._webdriver = Edge
+            self._service = Service(executable_path=driver_path)
+            print('Current Driver Edge')
         elif 'geckodriver' in driver_path:
             self._options = FirefoxOptions()
             self._webdriver = Firefox
+            self._service = FirefoxService(executable_path=driver_path)
+            print('Current Driver Firefox')
         else:
             raise ValueError("The driver is not supported. Please replace the driver")
-        if driver_options:
-            for each in driver_options:
-                self._options.add_argument(each)
-        self._driver_path = driver_path
+        # 加载配置
+        if driver_options:[self._options.add_argument(each) for each in driver_options]
 
     def start_browser(self):
-        browser = self._webdriver(service=Service(executable_path=self._driver_path), options=self._options)
-        # browser.implicitly_wait(6)
+        browser = self._webdriver(service=self._service, options=self._options)
+        # browser.implicitly_wait(2)
         browser.maximize_window()
         return browser
+
+if __name__ == '__main__':
+    driver = WebDriver(r'F:\Sdk\webdriver\geckodriver.exe')
+    driver.start_browser()
